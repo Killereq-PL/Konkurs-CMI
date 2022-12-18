4aec962984129400357e0284fddf9506dd0e1b2d
Killereq-PL <adam.kotek33@gmail.com>
2022-12-18 13:25:53 +0100

    update

--- 1. Redukcja Do Absurdu (RDA)/program.py
+++ 1. Redukcja Do Absurdu (RDA)/program.py
@@ -6,15 +6,23 @@ quantity = _inList[0]
 quantity = int(quantity)
 lineAmount = quantity*2
 _inList.pop(0)
-print('LICZENIE...')
+print('LICZENIE...\n')
 
-#for x in range(0, quantity):
+for x in range(quantity):
+    BuildingAmount = _inList[0]
+    _inList.pop(0)
+    BuildingString = str(_inList[0])
+    _inList.pop(0)
+    BuildingNumbers = BuildingString.split(sep=' ')
+    BuildingNumbers = [int(s) for s in BuildingNumbers]
+    print(*BuildingNumbers)
+
 
 _out.write("")
 _out.mode = "a"
-for y in range(0, lineAmount):
-    if y != 0:
-        _out.write('\n')
-    _out.write(_inList[y])
-    print(_inList[y])
-print('KONIEC')
+#for y in range(quantity):
+#    if y != 0:
+#        _out.write('\n')
+#    _out.write(_inList[y])
+#    print(_inList[y])
+print('\nKONIEC')
