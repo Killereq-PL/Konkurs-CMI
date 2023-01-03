ea63fc36567d08b08f6896e69e27d8a7eec993f9
Killereq-PL <adam.kotek33@gmail.com>
2023-01-03 20:57:44 +0100

    update

--- 2. Dodawanie (DOD)/program.py
+++ 2. Dodawanie (DOD)/program.py
@@ -1,21 +1,27 @@
-################################
-_in = open("in.txt", "r")      #
-_inDATA = _in.read()           #
-_out = open("out.txt", "w")    #
-_inList = _inDATA.splitlines() #
-quantity = _inList[0]          # Inicjalizacja
-quantity = int(quantity)       #
-_inList.pop(0)                 #
-print('LICZENIE...\n')         #
-Answers = []                   #
-################################
+##################################     
+try:                             #
+    _in = open("in.txt", "r")    #
+except FileNotFoundError:        #
+    _in = open("in.txt", "w")    #
+    _in.write('jeden\ndwa\ndwa') #
+    _in.mode = 'r'               #
+_inDATA = _in.read()             # Inicjalizacja
+_out = open("out.txt", "w")      #
+_inList = _inDATA.splitlines()   #
+quantity = _inList[0]            #
+_inList.pop(0)                   #
+print('LICZENIE...\n')           #
+Answers = []                     #
+##################################
+
 
 #miejsce na kod
+
 
 ######################################
 _out.write("") #wyczysc plik out.txt #
 _out.mode = "a"                      #
-for y in range(quantity):            #
+for y in range(len(Answers)):        #
     if y != 0:                       #
         _out.write('\n')             # Koniec
     print(Answers[y])                #
